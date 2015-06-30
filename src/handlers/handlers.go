137fd9aab9d678741cc2afad04172c07463b2cca
Antti Myllykoski <amyllykoski@yahoo.com>
2015-06-30 16:22:04 -0700

    unmarshalling images json correctly

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -31,12 +31,22 @@ func HandleHeartbeat(w http.ResponseWriter, r *http.Request) {
 		panic(err.Error())
 	}
 	fmt.Println("Body: " + string(body))
-	//	var data interface{}
 	var hb Heartbeat
 	json.Unmarshal(body, &hb)
 
 	fmt.Println(hb)
 	io.WriteString(w, "Hello hb!")
+}
+
+type Images []struct {
+	Created     uint64   `json:"Created"`
+	Id          string   `json:"Id"`
+	Labels      string   `json:"Labels"`
+	ParentId    string   `json:"ParentId"`
+	RepoDigests []string `json:"RepoDigests"`
+	RepoTags    []string `json:"RepoTags"`
+	Size        uint64   `json:"Size"`
+	VirtualSize uint64   `json:"VirtualSize"`
 }
 
 func HandleImages(w http.ResponseWriter, r *http.Request) {
@@ -46,31 +56,38 @@ func HandleImages(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("Body: " + string(body))
 
-	var images map[string]interface{}
-	json.Unmarshal([]byte(body), &images)
+	var images Images
+	json.Unmarshal(body, &images)
 
-	for k, v := range images {
-		switch vv := v.(type) {
-		case string:
-			fmt.Println(k, "is string", vv)
-		case int:
-			fmt.Println(k, "is int", vv)
-		case []interface{}:
-			fmt.Println(k, "is an array:")
-			for i, u := range vv {
-				fmt.Println(i, u)
-			}
-		default:
-			fmt.Println(k, "is of a type I don't know how to handle")
-		}
+	for _, image := range images {
+		fmt.Println("%v", image)
 	}
-
-	fmt.Println("%#v", images)
-
-	//	var data string
-	//	json.Unmarshal(body, &data)
-	//	fmt.Println("JSON: " + data)
 	io.WriteString(w, "Hello images!")
 }
 
 //	hb := data.(map[string]interface{})
+
+//for _, item := range strings.Split(string(body), ",") {
+//		if strings.Contains(string(item), "Created") {
+//			fmt.Println("%s", item)
+//		}
+//		if strings.Contains(string(item), "RepoTags") {
+//			fmt.Println("%s", item)
+//		}
+//	}
+
+//for k, v := range images {
+//		switch vv := v.(type) {
+//		case string:
+//			fmt.Println(k, "is string", vv)
+//		case int:
+//			fmt.Println(k, "is int", vv)
+//		case []interface{}:
+//			fmt.Println(k, "is an array:")
+//			for i, u := range vv {
+//				fmt.Println(i, u)
+//			}
+//		default:
+//			fmt.Println(k, "is of a type I don't know how to handle")
+//		}
+//	}
